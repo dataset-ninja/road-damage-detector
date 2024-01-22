8ada840e9f052a696e5f360cb8ff41eafb085fb5
peixed351 <peixed351@gmail.com>
2024-01-22 20:26:19 +0300

    add stats

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,15 @@
-import supervisely as sly
+import glob
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
+import xml.etree.ElementTree as ET
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import file_exists, get_file_name, get_file_name_with_ext
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +32,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +60,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +69,151 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = "/home/alex/DATASETS/TODO/RoadDamageDetector/RDD2022_all_countries"
+    batch_size = 30
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
+        tags = []
 
-    # ... some code here ...
+        folder = image_path.split("/")[-4]
+        tag_meta = folder_to_meta.get(folder)
+        tag_country = sly.Tag(tag_meta)
+        tags.append(tag_country)
+        if folder in ["China_Drone", "China_MotorBike"]:
+            view_value = folder.split("_")[1]
+            view_meta = folder_to_meta.get(view_value)
+            view = sly.Tag(view_meta)
+            tags.append(view)
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        ann_path = image_path.replace("images", "annotations/xmls").replace(".jpg", ".xml")
+        if file_exists(ann_path):
+            tree = ET.parse(ann_path)
+            root = tree.getroot()
+            img_wight = int(root.find(".//width").text)
+            img_height = int(root.find(".//height").text)
+            objects_content = root.findall(".//object")
+            for obj_data in objects_content:
+                label_tags = []
+                name = obj_data.find(".//name").text
+                obj_class = index_to_class.get(name)
+                if obj_class is not None:
+                    detail_value = index_to_value.get(name)
+                    if detail_value is not None:
+                        detail = sly.Tag(detail_meta, value=detail_value)
+                        label_tags.append(detail)
+                    bndbox = obj_data.find(".//bndbox")
+                    top = float(bndbox.find(".//ymin").text)
+                    left = float(bndbox.find(".//xmin").text)
+                    bottom = float(bndbox.find(".//ymax").text)
+                    right = float(bndbox.find(".//xmax").text)
 
-    # return project
+                    rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                    label = sly.Label(rectangle, obj_class, tags=label_tags)
+                    labels.append(label)
 
+            return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
 
+    longitudinal = sly.ObjClass("longitudinal crack", sly.Rectangle)
+    transverse = sly.ObjClass("transverse crack", sly.Rectangle)
+    alligator = sly.ObjClass("alligator crack", sly.Rectangle)
+    pothole = sly.ObjClass("pothole", sly.Rectangle)
+    other = sly.ObjClass("other corruption", sly.Rectangle)
+    block = sly.ObjClass("block crack", sly.Rectangle)
+    repair = sly.ObjClass("repair", sly.Rectangle)
+
+    index_to_class = {
+        "D00": longitudinal,
+        "D01": longitudinal,
+        "D0w0": longitudinal,
+        "D10": transverse,
+        "D11": transverse,
+        "D20": alligator,
+        "D40": pothole,
+        "D43": other,
+        "D44": other,
+    }
+
+    china_meta = sly.TagMeta("China", sly.TagValueType.NONE)
+    drone_meta = sly.TagMeta("drone", sly.TagValueType.NONE)
+    motorbike_meta = sly.TagMeta("motorbike", sly.TagValueType.NONE)
+    czech_meta = sly.TagMeta("Czech", sly.TagValueType.NONE)
+    india_meta = sly.TagMeta("India", sly.TagValueType.NONE)
+    japan_meta = sly.TagMeta("Japan", sly.TagValueType.NONE)
+    norway_meta = sly.TagMeta("Norway", sly.TagValueType.NONE)
+    us_meta = sly.TagMeta("United States", sly.TagValueType.NONE)
+    detail_meta = sly.TagMeta("detail", sly.TagValueType.ANY_STRING)
+
+    folder_to_meta = {
+        "China_Drone": china_meta,
+        "China_MotorBike": china_meta,
+        "Czech": czech_meta,
+        "India": india_meta,
+        "Japan": japan_meta,
+        "Norway": norway_meta,
+        "United_States": us_meta,
+        "Drone": drone_meta,
+        "MotorBike": motorbike_meta,
+    }
+
+    china_meta = sly.TagMeta("China", sly.TagValueType.NONE)
+
+    index_to_value = {
+        "D00": "wheel mark part",
+        "D01": "construction joint part",
+        "D10": "equal interval",
+        "D11": "construction joint part",
+        "D20": "partial pavement, overall pavement",
+        "D40": "rutting, bump, pothole, separation",
+        "D43": "crosswalk blur",
+        "D44": "white line blur",
+    }
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[longitudinal, transverse, alligator, pothole, other, block, repair],
+        tag_metas=[
+            china_meta,
+            drone_meta,
+            motorbike_meta,
+            czech_meta,
+            india_meta,
+            japan_meta,
+            norway_meta,
+            us_meta,
+            detail_meta,
+        ],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    train_images_pathes = glob.glob(dataset_path + "/*/train/images/*.jpg")
+    test_images_pathes = glob.glob(dataset_path + "/*/test/images/*.jpg")
+
+    ds_name_to_data = {"train": train_images_pathes, "test": test_images_pathes}
+
+    for ds_name, images_pathes in ds_name_to_data.items():
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+        for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+            images_names_batch = [
+                get_file_name_with_ext(image_path) for image_path in img_pathes_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            if ds_name == "train":
+                anns_batch = [create_ann(image_path) for image_path in img_pathes_batch]
+                api.annotation.upload_anns(img_ids, anns_batch)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,39 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Road Damage Detection 2022"
+PROJECT_NAME_FULL: str = "The multi-national Road Damage Dataset 2022"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC_BY_SA_4_0(source_url="https://crddc2022.sekilab.global/rules/")
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Automotive()]
+CATEGORY: Category = Category.SelfDriving()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2022-08-12"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://crddc2022.sekilab.global/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 13085047
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/road-damage-detector"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/RDD2022.zip"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,15 +53,27 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[
+    Union[str, List[str], Dict[str, str]]
+] = "https://www.researchgate.net/publication/363668453_RDD2022_A_multi-national_image_dataset_for_automatic_Road_Damage_Detection"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {
+    "GitHub": "https://github.com/sekilab/RoadDamageDetector?tab=readme-ov-file",
+    "Figshare": "https://figshare.com/articles/dataset/RDD2022_-_The_multi-national_Road_Damage_Dataset_released_through_CRDDC_2022/21431547",
+}
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = [
+    "Deeksha Arya",
+    "Hiroya Maeda",
+    "Yoshihide Sekimoto",
+    "Hiroshi Omata",
+    "Sanjay Kumar Ghosh",
+    "Durga Toshniwal",
+]
+AUTHORS_CONTACTS: Optional[List[str]] = ["deeksha@ct.iitr.ac.in"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = ["JP-ID joint research group"]
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
